01d5481971f8e163b4a1a137190b60a549115da0
GreenLantern101 <arctangent20@yahoo.com>
2017-02-01 13:47:40 -0600

    Eliminate nested if-else loops
    Use more readable code style

--- UnityMultiplayer/Assets/script/TargetPosition.cs
+++ UnityMultiplayer/Assets/script/TargetPosition.cs
@@ -1,13 +1,15 @@
 ﻿using UnityEngine;
 
-public class TargetPosition: MonoBehaviour{
+public class TargetPosition: MonoBehaviour
+{
 	private float MoveRate = 20f;
 	private bool moveFlag = false;
 	private float threshUp;
 	private float threshDown;
 
-	void Start(){
-		if (gameObject.name == "Target1T" || gameObject.name == "Target2T" ) {
+	void Start()
+	{
+		if (gameObject.name == "Target1T" || gameObject.name == "Target2T") {
 			threshUp = -98f;
 			threshDown = -160f;
 		} else {
@@ -16,36 +18,38 @@ public class TargetPosition: MonoBehaviour{
 		}
 	}
 
-	void Update(){
+	void Update()
+	{
+		if (!MainController.FSM.IsInState(PuzzleState.GAME_STEP))
+			return;
 		
 		Vector3 curPosition = gameObject.transform.position;
-		if (MainController.FSM.IsInState (PuzzleState.GAME_STEP)) {
-			if(gameObject.name == "PlayArea"){
-				Vector3 targetPosition = GameObject.Find(GameController.targetTName).transform.position;
-				targetPosition.z -= (float)GameInfo.gapHT[GameController.targetTName];
-				targetPosition.y = 0f;
-				gameObject.transform.position = targetPosition;
-			}else{
-				if (gameObject.name == GameController.targetTName) {
-					if (MainController.curGameNum == 7 || MainController.curGameNum == 8 || MainController.curGameNum == 9) {						
-						GameController.setPosition("");
-						if ((curPosition.z > threshUp && !moveFlag) || (curPosition.z < threshDown && moveFlag)) {
-								moveFlag = !moveFlag;
-								MoveRate = 0 - MoveRate;
-						}
-
-						curPosition.z += Time.deltaTime * MoveRate;
-						curPosition.x += Time.deltaTime * MoveRate * Random.Range(0f, 0.5f);
-						gameObject.transform.position = curPosition;
-					} else {
-							curPosition.z = (float)GameInfo.initialPositionArray[GameController.targetTName];
-							gameObject.transform.position = curPosition;					
-					}
-				} else {
-					curPosition.z = 300f;
-					gameObject.transform.position = curPosition;						
+		
+		if (gameObject.name == "PlayArea") {
+			Vector3 targetPosition = GameObject.Find(GameController.targetTName).transform.position;
+			targetPosition.z -= (float)GameInfo.gapHT[GameController.targetTName];
+			targetPosition.y = 0f;
+			gameObject.transform.position = targetPosition;
+			return;
+		}
+		if (gameObject.name == GameController.targetTName) {
+			if (MainController.curGameNum == 7 || MainController.curGameNum == 8 || MainController.curGameNum == 9) {						
+				GameController.setPosition("");
+				if ((curPosition.z > threshUp && !moveFlag) || (curPosition.z < threshDown && moveFlag)) {
+					moveFlag = !moveFlag;
+					MoveRate = 0 - MoveRate;
 				}
+
+				curPosition.z += Time.deltaTime * MoveRate;
+				curPosition.x += Time.deltaTime * MoveRate * Random.Range(0f, 0.5f);
+				gameObject.transform.position = curPosition;
+			} else {
+				curPosition.z = (float)GameInfo.initialPositionArray[GameController.targetTName];
+				gameObject.transform.position = curPosition;					
 			}
+		} else {
+			curPosition.z = 300f;
+			gameObject.transform.position = curPosition;						
 		}
 	}
 }
