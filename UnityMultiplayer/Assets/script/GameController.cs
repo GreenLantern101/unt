5b9dfcf26afc6e7f7189b440b724cc0fc772b633
GreenLantern101 <arctangent20@yahoo.com>
2017-02-02 16:59:26 -0600

    Set block succeed flag to true on success sync

--- UnityMultiplayer/Assets/script/GameController.cs
+++ UnityMultiplayer/Assets/script/GameController.cs
@@ -355,18 +355,16 @@ public class GameController : MonoBehaviour
 				
 				//sync block success from networked player
 				if (blocksuccess_index > -1) {
+					GameObject obj = GameInfo.blockList[blocksuccess_index];
+					//move final orient
 					float y = GameInfo.getTargetRotation(blocksuccess_index);
 					Vector3 finalOrient = new Vector3(0f, y, 0f);
-					
-					GameObject obj = GameInfo.blockList[blocksuccess_index];
-					Debug.Log("old ang: " + obj.transform.localEulerAngles);
 					obj.transform.localEulerAngles = finalOrient;
-					
+					//move final pos
 					Vector3 finalpos = GameInfo.getTargetPosition(blocksuccess_index);
-					Debug.Log("old pos: " + obj.transform.position);
 					obj.transform.position = finalpos;
-					Debug.Log("POS: " + finalpos);
-					Debug.Log("ORIENT: " + finalOrient);
+					//set block succeed flag
+					GameInfo.blockSucceed[blocksuccess_index] = true;
 					//reset
 					blocksuccess_index = -1;
 				}
