6f9ff59ce72c29b700cf3122f729ad8666fa2f33
GreenLantern101 <arctangent20@yahoo.com>
2017-02-06 16:32:52 -0600

    Connect to python external server

--- UnityMultiplayer/Assets/script/NetworkConnection.cs
+++ UnityMultiplayer/Assets/script/NetworkConnection.cs
@@ -16,77 +16,91 @@ public class NetworkConnection: MonoBehaviour
 	private int pPort;
 	private TcpClient speechClient;
 
+	private TcpClient serverClient;
+
 	//location of google server for AI
-	private string Address = "129.59.79.186";
-	//private string Address = "10.110.10.208";
+	//private string Address = "129.59.79.186";
+	private string Address = "129.59.79.171;
 
 
 	public Hashtable speechHT;
 	NetworkStream netStream;
 
-	void Start() {
+	void Start()
+	{
 		//start speech connection
-		/*
-		print ("Connecting to speech started.");
-	    StartSpeechConnection();
-		print ("Connected to speech finished.");
-		netStream = speechClient.GetStream ();
-		*/
-		MainController.FSM.Fire (Trigger.startIntro);
+		
+		Debug.Log("Connecting to speech started.");
+		StartSpeechConnection();
+		Debug.Log("Connected to speech finished.");
+		netStream = speechClient.GetStream();
+		
+		MainController.FSM.Fire(Trigger.startIntro);
 		//start player connection
-//		pPort = 3000;
+		pPort = 10011;
 //		pData = new PlayerData();
-//		StartPlayerServer ();
-//		print ("Connection to player finished");
+		StartPlayerServer();
+		Debug.Log("Connection to player finished");
 	}
 
-	void Update(){
+	void Update()
+	{
 		//ensure speechClient not null
 		if (speechClient == null)
 			return;
 		if (speechClient.Connected && MainController.FSM.IsInState(PuzzleState.GAME)) {            
 
-				if(speechClient.Available>0){
-					string speechDataString = receiveSpeechData();
-					print ("the received data is: " + speechDataString);
-					decodeSpeech(speechDataString);
-					LanguageManager.takeAction(speechHT);
-				}
+			if (speechClient.Available > 0) {
+				string speechDataString = receiveSpeechData();
+				print("the received data is: " + speechDataString);
+				decodeSpeech(speechDataString);
+				LanguageManager.takeAction(speechHT);
 			}
 		}
+	}
+
+	void StartPlayerServer()
+	{
+		pPort = 10011;
+		serverClient = new TcpClient();
+		serverClient.Connect(Address, pPort);
+
+	}
 
 	// Use this to start the server for speech recognition
 	void StartSpeechConnection()
 	{            
 		sPort = 10010;
-		speechClient = new TcpClient ();
-		speechClient.Connect (Address, sPort);
+		speechClient = new TcpClient();
+		speechClient.Connect(Address, sPort);
 	}
 
-	string receiveSpeechData(){
+	string receiveSpeechData()
+	{
 		byte[] bytes = new byte[speechClient.ReceiveBufferSize];
-		netStream.Read (bytes, 0, (int)speechClient.ReceiveBufferSize);
-		string returndata = Encoding.UTF8.GetString (bytes);
+		netStream.Read(bytes, 0, (int)speechClient.ReceiveBufferSize);
+		string returndata = Encoding.UTF8.GetString(bytes);
 		return returndata;
 	}
 
-	void decodeSpeech(string _str){
-		speechHT = new Hashtable ();
-		print ("received data: " + _str);
+	void decodeSpeech(string _str)
+	{
+		speechHT = new Hashtable();
+		print("received data: " + _str);
 		//each element is ended with ','
 		string[] elements = _str.Split(',');
-		for (int i=0; i<elements.Length; ++i) {
-			if(!elements[i].Contains(':'))
+		for (int i = 0; i < elements.Length; ++i) {
+			if (!elements[i].Contains(':'))
 				continue;
 
 			string[] Elem = elements[i].Split(':');
 
-			if(Elem.Length != 2)
+			if (Elem.Length != 2)
 				continue;
-			if(speechHT.ContainsKey(Elem[0])){
+			if (speechHT.ContainsKey(Elem[0])) {
 				speechHT[Elem[0]] = Elem[1];
-			}else{
-				speechHT.Add (Elem [0], Elem [1]);
+			} else {
+				speechHT.Add(Elem[0], Elem[1]);
 			}
 		}
 	}
