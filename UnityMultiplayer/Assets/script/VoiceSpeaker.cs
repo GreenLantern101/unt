e2577174adf180856447d4b2f2ed76e97bacfe78
GreenLantern101 <arctangent20@yahoo.com>
2017-03-01 16:09:02 -0600

    Fix indentation

--- UnityMultiplayer/Assets/script/VoiceSpeaker.cs
+++ UnityMultiplayer/Assets/script/VoiceSpeaker.cs
@@ -13,39 +13,40 @@ using System.Runtime.InteropServices;
 
 public class VoiceSpeaker : MonoBehaviour
 {
-	[DllImport ("Voice_speaker.dll", EntryPoint="VoiceAvailable")] private static extern int    VoiceAvailable();
-	[DllImport ("Voice_speaker.dll", EntryPoint="InitVoice")]      private static extern void   InitVoice();
-	[DllImport ("Voice_speaker.dll", EntryPoint="WaitUntilDone")]  private static extern int    WaitUntilDone(int millisec);
-	[DllImport ("Voice_speaker.dll", EntryPoint="FreeVoice")]      private static extern void   FreeVoice();
-	[DllImport ("Voice_speaker.dll", EntryPoint="GetVoiceCount")]  private static extern int    GetVoiceCount();
+	[DllImport("Voice_speaker.dll", EntryPoint = "VoiceAvailable")] private static extern int    VoiceAvailable();
+	[DllImport("Voice_speaker.dll", EntryPoint = "InitVoice")]      private static extern void   InitVoice();
+	[DllImport("Voice_speaker.dll", EntryPoint = "WaitUntilDone")]  private static extern int    WaitUntilDone(int millisec);
+	[DllImport("Voice_speaker.dll", EntryPoint = "FreeVoice")]      private static extern void   FreeVoice();
+	[DllImport("Voice_speaker.dll", EntryPoint = "GetVoiceCount")]  private static extern int    GetVoiceCount();
 	
 	// Unity V4.x.x
-	[DllImport ("Voice_speaker.dll", EntryPoint="GetVoiceName")]   private static extern IntPtr GetVoiceName(int index);
+	[DllImport("Voice_speaker.dll", EntryPoint = "GetVoiceName")]   private static extern IntPtr GetVoiceName(int index);
 	//  other Unity version
 	// [DllImport ("Voice_speaker.dll", EntryPoint="GetVoiceName")]   private static extern string GetVoiceName(int index);
 	
-	[DllImport ("Voice_speaker.dll", EntryPoint="SetVoice")]       private static extern void   SetVoice(int index);
-	[DllImport ("Voice_speaker.dll", EntryPoint="Say")]            private static extern void   Say(string ttospeak);
-	[DllImport ("Voice_speaker.dll", EntryPoint="SayAndWait")]     private static extern void   SayAndWait(string ttospeak);
-	[DllImport ("Voice_speaker.dll", EntryPoint="SpeakToFile")]    private static extern int    SpeakToFile(string filename, string ttospeak);
-	[DllImport ("Voice_speaker.dll", EntryPoint="GetVoiceState")]  private static extern int    GetVoiceState();
-	[DllImport ("Voice_speaker.dll", EntryPoint="GetVoiceVolume")] private static extern int    GetVoiceVolume();
-	[DllImport ("Voice_speaker.dll", EntryPoint="SetVoiceVolume")] private static extern void   SetVoiceVolume(int volume);
-	[DllImport ("Voice_speaker.dll", EntryPoint="GetVoiceRate")]   private static extern int    GetVoiceRate();
-	[DllImport ("Voice_speaker.dll", EntryPoint="SetVoiceRate")]   private static extern void   SetVoiceRate(int rate);
-	[DllImport ("Voice_speaker.dll", EntryPoint="PauseVoice")]     private static extern void   PauseVoice();
-	[DllImport ("Voice_speaker.dll", EntryPoint="ResumeVoice")]    private static extern void   ResumeVoice();
+	[DllImport("Voice_speaker.dll", EntryPoint = "SetVoice")]       private static extern void   SetVoice(int index);
+	[DllImport("Voice_speaker.dll", EntryPoint = "Say")]            private static extern void   Say(string ttospeak);
+	[DllImport("Voice_speaker.dll", EntryPoint = "SayAndWait")]     private static extern void   SayAndWait(string ttospeak);
+	[DllImport("Voice_speaker.dll", EntryPoint = "SpeakToFile")]    private static extern int    SpeakToFile(string filename, string ttospeak);
+	[DllImport("Voice_speaker.dll", EntryPoint = "GetVoiceState")]  private static extern int    GetVoiceState();
+	[DllImport("Voice_speaker.dll", EntryPoint = "GetVoiceVolume")] private static extern int    GetVoiceVolume();
+	[DllImport("Voice_speaker.dll", EntryPoint = "SetVoiceVolume")] private static extern void   SetVoiceVolume(int volume);
+	[DllImport("Voice_speaker.dll", EntryPoint = "GetVoiceRate")]   private static extern int    GetVoiceRate();
+	[DllImport("Voice_speaker.dll", EntryPoint = "SetVoiceRate")]   private static extern void   SetVoiceRate(int rate);
+	[DllImport("Voice_speaker.dll", EntryPoint = "PauseVoice")]     private static extern void   PauseVoice();
+	[DllImport("Voice_speaker.dll", EntryPoint = "ResumeVoice")]    private static extern void   ResumeVoice();
 	
 	public int voice_nb = 0;
 	
-	void Start ()
+	void Start()
 	{
-		if( VoiceAvailable()>0 )
-		{
+		if (VoiceAvailable() > 0) {
 			InitVoice(); // init the engine
 			
-			if (voice_nb > GetVoiceCount()) voice_nb = 0;
-			if (voice_nb < 0) voice_nb = 0;
+			if (voice_nb > GetVoiceCount())
+				voice_nb = 0;
+			if (voice_nb < 0)
+				voice_nb = 0;
 			
 			// Unity V4.x.x *******************************************
 			IntPtr pStr = GetVoiceName(voice_nb);
@@ -55,11 +56,11 @@ public class VoiceSpeaker : MonoBehaviour
 			
 			//Debug.Log ("Voice name : "+GetVoiceName(voice_nb)); // Voice Name other Unity version
 			
-			Debug.Log ("Number of voice : "+ GetVoiceCount()); // Number of voice
+			Debug.Log("Number of voice : " + GetVoiceCount()); // Number of voice
 			
 			SetVoice(voice_nb); // 0 to voiceCount - 1
 			SetVoiceRate(0);
-			Debug.Log ("Voice Rate : "+GetVoiceRate());
+			Debug.Log("Voice Rate : " + GetVoiceRate());
 			
 			//Debug.Log ("Voice name : "+GetVoiceName(voice_nb));
 			//Say("All system nominal. Engine, online, weapons, online.. We are ready. 9.,.8.,.7.,.6.,.5.,.4.,.3.,.2.,.1.,.0.,. Go,.Take off");
@@ -70,15 +71,16 @@ public class VoiceSpeaker : MonoBehaviour
 	
 	void OnDisable()
 	{
-		if( VoiceAvailable()>0 )
-		{
+		if (VoiceAvailable() > 0) {
 			FreeVoice();
 		}
 	}
 
-	public static void speakOut(string _str){
-		LogTimeData.logEvent (LogTimeData.speakEvnet);
-		//Say (_str);
+	public static void speakOut(string _str)
+	{
+		LogTimeData.logEvent(LogTimeData.speakEvnet);
+		Debug.Log("ATTEMPTED TO SAY: " + _str);
+		Say(_str);
 		LanguageManager.responseText = "";
 	}
 
