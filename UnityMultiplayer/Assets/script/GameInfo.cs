fe9bbbd040e3c42153c2ed9963fed39f061bef47
GreenLantern101 <arctangent20@yahoo.com>
2017-03-14 11:53:13 -0500

    add random seed variation for player-player
    as opposed to player-agent

--- UnityMultiplayer/Assets/script/GameInfo.cs
+++ UnityMultiplayer/Assets/script/GameInfo.cs
@@ -311,7 +311,7 @@ public static class GameInfo
 		
 		//only is synchronized if two players are not off by more than 20 seconds
 		int salt = (int)GameController.agent_player_switch_timer / 20;
-		int seed = 29* MainController.curGameNum + salt + 70;
+		int seed = 29 * MainController.curGameNum + 17 * (MainController.gameOdd ? 1 : 0) + salt + 70;
 		
 		Debug.Log("SEED: " + seed);
 		System.Random rnd = new System.Random(seed);
